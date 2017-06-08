2ef22285c0a96c5e3683006c45fc992e29fba32e
prcer <paulo_cerioni@hotmail.com>
2017-06-07 22:32:39 -0300

    Improve documentation

--- demo.py
+++ demo.py
@@ -5,10 +5,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
 clf = tree.DecisionTreeClassifier()
-# CHALLENGE - create 3 more classifiers...
-# 1
-# 2
-# 3
+# CHALLENGE - create 3 more classifiers
 clf1 = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
 clf2 = SVC(kernel = 'linear', random_state = 0)
 clf3 = LogisticRegression(random_state = 0)
@@ -27,28 +24,37 @@ clf1 = clf1.fit(X, Y)
 clf2 = clf2.fit(X, Y)
 clf3 = clf3.fit(X, Y)
 
-prediction1 = clf1.predict([[190, 70, 43], [150, 70, 30], [170, 80, 38], [200, 90, 44], [160, 50, 32]])
-prediction2 = clf2.predict([[190, 70, 43], [150, 70, 30], [170, 80, 38], [200, 90, 44], [160, 50, 32]])
-prediction3 = clf3.predict([[190, 70, 43], [150, 70, 30], [170, 80, 38], [200, 90, 44], [160, 50, 32]])
+# Predictions vector to test the models (chosen independently from the training set)
+Ytest = [[190, 70, 43], [150, 70, 30], [170, 80, 38], [200, 90, 44], [160, 50, 32]]
+
+# Predit genders for samples in the test set for each classifier
+prediction = clf.predict(Ytest)
+prediction1 = clf1.predict(Ytest)
+prediction2 = clf2.predict(Ytest)
+prediction3 = clf3.predict(Ytest)
+
+# Ground truth for the predictions
 predTrue = ['male', 'female', 'male', 'male', 'female']
 
-print(prediction1)
-print(prediction2)
-print(prediction3)
-
-# CHALLENGE compare their results and print the best one!
+# Calculate accuracy for each classifier
 s1 = accuracy_score(predTrue, prediction1)
 s2 = accuracy_score(predTrue, prediction2)
 s3 = accuracy_score(predTrue, prediction3)
 
-print(s1)
-print(s2)
-print(s3)
+# CHALLENGE: compare their results and print the best one!
+
+# Print predicted values and accuracy in % for each classifier
+print('RandomForestClassifier: ', s1 * 100, '%')
+print(prediction1)
+print('SVC: ', s2 * 100, '%')
+print(prediction2)
+print('LogisticRegression ', s3 * 100, '%')
+print(prediction3)
 
 # Print the best classifier
 if s1 >= s2 and s1 >= s3:
-    print('RandomForestClassifier')
+    print('Best Classifier: RandomForestClassifier')
 elif s2 >= s1 and s2 >= s3:
-    print('SVC')
+    print('Best Classifier: SVC')
 else:
-    print('LogisticRegression')
+    print('Best Classifier: LogisticRegression')
